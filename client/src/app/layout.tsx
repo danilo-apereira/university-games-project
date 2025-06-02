1d115851d22f7286813484c9880f9645917388de
Danilo Pereira <contato.daniloapereira@gmail.com>
2025-06-01 23:29:03 -0300

    feat: estrutura básica do projeto

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "University Games Project",
+  description:
+    "Um projeto universitário que simula a listagem e avaliação de jogos, com dados obtidos por meio de uma API.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
